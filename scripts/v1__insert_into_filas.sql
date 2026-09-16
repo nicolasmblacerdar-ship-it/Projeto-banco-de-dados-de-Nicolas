4a4df389e44a1532cb65264750fb9db063bcf702
nicolasmblacerdar-ship-it <nicolasmblacerdar@gmail.com>
2026-09-15 21:49:13 -0300

    Update v1__insert_into_filas.sql

--- scripts/v1__insert_into_filas.sql
+++ scripts/v1__insert_into_filas.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO filas (nome, descricao) VALUES
+('Atendimento Geral', 'Fila para triagem e dúvidas gerais'),
+('Preferencial', 'Fila para idosos, gestantes e PCD'),
+('Suporte Técnico', 'Fila para resolução de problemas com serviços')
+ON CONFLICT (nome) DO NOTHING;
