990afed94c0f8d2604562c64be5b4e05e8879f17
nicolasmblacerdar-ship-it <nicolasmblacerdar@gmail.com>
2026-09-15 21:47:22 -0300

    Update v1__create_table_atendente_fila.sql

--- scripts/v1__create_table_atendente_fila.sql
+++ scripts/v1__create_table_atendente_fila.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS atendente_fila (
+    pessoa_id BIGINT NOT NULL,
+    fila_id BIGINT NOT NULL,
+    PRIMARY KEY (pessoa_id, fila_id),
+    CONSTRAINT fk_atendente_pessoa FOREIGN KEY (pessoa_id) REFERENCES pessoas(id) ON DELETE CASCADE,
+    CONSTRAINT fk_atendente_fila FOREIGN KEY (fila_id) REFERENCES filas(id) ON DELETE CASCADE
+);
