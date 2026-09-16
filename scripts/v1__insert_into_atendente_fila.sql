cb15ab2794048edb164d712ec4596d7291a3c8c6
nicolasmblacerdar-ship-it <nicolasmblacerdar@gmail.com>
2026-09-15 21:49:48 -0300

    Update v1__insert_into_atendente_fila.sql

--- scripts/v1__insert_into_atendente_fila.sql
+++ scripts/v1__insert_into_atendente_fila.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO atendente_fila (pessoa_id, fila_id) VALUES
+(1, 1), -- Carlos na Fila Geral
+(1, 2), -- Carlos na Fila Preferencial
+(2, 3)  -- Mariana no Suporte Técnico
+ON CONFLICT DO NOTHING;
