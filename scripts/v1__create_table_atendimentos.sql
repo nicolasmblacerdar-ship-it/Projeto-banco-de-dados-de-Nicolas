a7162e045d41af12ef0718ea4096610d0478df15
nicolasmblacerdar-ship-it <nicolasmblacerdar@gmail.com>
2026-09-15 21:48:00 -0300

    Update v1__create_table_atendimentos.sql

--- scripts/v1__create_table_atendimentos.sql
+++ scripts/v1__create_table_atendimentos.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE IF NOT EXISTS atendimentos (
+    id BIGSERIAL PRIMARY KEY,
+    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fila_id BIGINT NOT NULL,
+    atendente_id BIGINT NOT NULL,
+    cliente_id BIGINT NOT NULL,
+    status VARCHAR(20) DEFAULT 'Em Andamento',
+    CONSTRAINT fk_atendimento_fila FOREIGN KEY (fila_id) REFERENCES filas(id),
+    CONSTRAINT fk_atendimento_atendente FOREIGN KEY (atendente_id) REFERENCES pessoas(id),
+    CONSTRAINT fk_atendimento_cliente FOREIGN KEY (cliente_id) REFERENCES pessoas(id)
+);
